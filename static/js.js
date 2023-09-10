2bba0f889e1511eecda624202508cb6c8d372dc7
chris <chrisj21283@gmail.com>
2023-09-10 13:23:56 -0400

    modified the display and wrapper

--- static/js.js
+++ static/js.js
@@ -1,30 +1,37 @@
-let inputval = document.querySelector('#cityinput')
+let inputval = document.querySelector('#cityinput');
 let btn = document.querySelector('#add');
-let city = document.querySelector('#cityoutput')
-let descrip = document.querySelector('#description')
-let temp = document.querySelector('#temp')
-let wind = document.querySelector('#wind')
-apik = "51ba52544eb7da4bbab62a6b3b2cfd38"
+let city = document.querySelector('#cityoutput');
+let descrip = document.querySelector('#description');
+let temp = document.querySelector('#temp');
+let feeltemp = document.querySelector('#feeltemp');
+let wind = document.querySelector('#wind');
+let humidity = document.querySelector('#humidity'); 
+apik = "51ba52544eb7da4bbab62a6b3b2cfd38";
+
 function convertion(val) {
-  return ((val - 273.15) * 9 / 5 + 32).toFixed(2)
+  return ((val - 273.15) * 9 / 5 + 32).toFixed(2);
 }
+
 
 btn.addEventListener('click', function () {
   fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputval.value + '&appid=' + apik)
     .then(res => res.json())
 
-
     .then(data => {
-      let nameval = data['name']
-      let descrip = data['weather']['0']['description']
-      let tempature = data['main']['temp']
-      let wndspd = data['wind']['speed']
-      city.innerHTML = `Weather of <span>${nameval}<span>`
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} F</span>`
-      description.innerHTML = `Sky Conditions: <span>${descrip}<span>`
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`
+      let nameval = data['name'];
+      let descripVal = data['weather']['0']['description']; 
+      let tempature = data['main']['temp'];
+      let feeltempature = data['main']['feels_like'];
+      let wndspd = data['wind']['speed'];
+      let humidityVal = data['main']['humidity']; 
+      city.innerHTML = `Weather of <span>${nameval}</span>`;
+      temp.innerHTML = `Temperature: <span>${convertion(tempature)} F</span>`;
+      feeltemp.innerHTML = `Feels Like Temperature: <span>${convertion(feeltempature)} F</span>`;
+      descrip.innerHTML = `Sky Conditions: <span>${descripVal}</span>`;
+      wind.innerHTML = `Wind Speed: <span>${(wndspd * 2.23694).toFixed(2)} mph</span>`;
+
+      humidity.innerHTML = `Humidity: <span>${humidityVal}%</span>`; 
 
     })
-
-    .catch(err => alert('You entered Wrong city name'))
-})
+    .catch(err => alert('No city found with that name, try again'));
+});
