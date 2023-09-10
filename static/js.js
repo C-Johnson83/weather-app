64562f7a9ed07c03715101bbd3f49dc719e20f66
chris <chrisj21283@gmail.com>
2023-09-10 15:33:03 -0400

    Added some more data rows to the output

--- static/js.js
+++ static/js.js
@@ -6,6 +6,8 @@ let temp = document.querySelector('#temp');
 let feeltemp = document.querySelector('#feeltemp');
 let wind = document.querySelector('#wind');
 let humidity = document.querySelector('#humidity'); 
+let pressure = document.querySelector('#pressure'); 
+let cloudy = document.querySelector('#cloudy'); 
 apik = "51ba52544eb7da4bbab62a6b3b2cfd38";
 
 function convertion(val) {
@@ -23,14 +25,18 @@ btn.addEventListener('click', function () {
       let tempature = data['main']['temp'];
       let feeltempature = data['main']['feels_like'];
       let wndspd = data['wind']['speed'];
-      let humidityVal = data['main']['humidity']; 
+      let humidityVal = data['main']['humidity'];
+      let pressVal = data['main']['pressure'];
+      let cloudVal =data['clouds']['all'];
       city.innerHTML = `Weather of <span>${nameval}</span>`;
       temp.innerHTML = `Temperature: <span>${convertion(tempature)} F</span>`;
       feeltemp.innerHTML = `Feels Like Temperature: <span>${convertion(feeltempature)} F</span>`;
       descrip.innerHTML = `Sky Conditions: <span>${descripVal}</span>`;
+      cloudy.innerHTML = `Cloudiness: <span>${cloudVal} %</span>`;
       wind.innerHTML = `Wind Speed: <span>${(wndspd * 2.23694).toFixed(2)} mph</span>`;
-
       humidity.innerHTML = `Humidity: <span>${humidityVal}%</span>`; 
+      pressure.innerHTML = `Atmospheric Pressure: <span>${pressVal} hPa</span>`;
+     
 
     })
     .catch(err => alert('No city found with that name, try again'));
